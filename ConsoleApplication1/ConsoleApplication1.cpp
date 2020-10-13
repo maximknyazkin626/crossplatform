3631dc517ce07ae70f9b154106096faef6df6aba
maximknyazkin626 <knyazkin.maxim.626@gmail.com>
2020-10-13 23:24:40 +0300

    Commit 2

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,10 +3,17 @@
 
 #include <iostream>
 using namespace std;
+
+bool check_value(int x) {
+    return x > 0 ? true : false;
+}
+bool check_value(double x) {
+    return x > 0 ? true : false;
+}
 struct pipe {
     string id;
     double dlina;
-    int diametr;
+    double diametr;
     bool remont;
 };
 
@@ -14,25 +21,68 @@ struct ks {
     string id;
     string name;
     int ceh;
-    int cehr;
-    double eff;
+    int ceh_inwork;
+    double effect;
 };
 
 pipe InputPipe ()
 {
     pipe p;
-    cout << "Type id: ";
-    cin >> p.id;
+    cout << "Введите диаметр: ";
+    cin >> p.diametr;
+    cin.clear();
+    cin.ignore(10000, '\n');
+    cout << "Введит длину: " ;
+    cin >> p.dlina;
+    p.remont = false;
+    p.id = -1;
+
+
     return p;
 }
 
+ks InputKs()
+{
+    ks new_ks;
+    cout << "Введите имя: " << endl;
+    cin >> new_ks.name;
+    cin.clear();
+    cin.ignore(10000, '\n');
+    cout << "Введите кол-во цехов: " << endl;
+    cin >> new_ks.ceh;
+    cin.clear();
+    cin.ignore(10000, '\n');
+    cout << "Введите кол-во рабочих цехов: " << endl;
+    cin >> new_ks.ceh_inwork;
+    cin.clear();
+    cin.ignore(10000, '\n');
+    cout << "Введите эффективность: " << endl;
+    cin >> new_ks.effect;
+    new_ks.id = -1;
+
+    
+
+    return new_ks;
+}
+
+
 void PrintPipe(pipe p)
 {
-    cout << "id: " << p.id << endl;
+    cout << "Диаметр: " << p.diametr << endl;
+    cout << "Длина: " << p.dlina << endl;
+}
+
+void PrintKs(ks new_ks)
+{
+    cout << "Имя:" << new_ks.name << endl;
+    cout << "Кол-во цехов: " << new_ks.ceh << endl;
+    cout << "Кол-во рабочих цехов: " << new_ks.ceh_inwork << endl;
+    cout << "Эффективность: " << new_ks.effect << endl;
 }
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
     string s;
     ///cout << "Type your name,please" << endl;
     //cin >> s;
@@ -40,7 +90,8 @@ int main()
 
     pipe pi = InputPipe();
     PrintPipe(pi);
-
+    ks kompresor = InputKs();
+    PrintKs(kompresor);
 
 }
 
